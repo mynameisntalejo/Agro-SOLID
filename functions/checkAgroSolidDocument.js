ca2247af56a18579a265152de47dd08c40f54c6d
Alejo Marin Iparraguirre <alejomariniparraguirre@gmail.com>
2020-12-23 17:51:34 -0300

    Correccion path documents

--- functions/checkAgroSolidDocument.js
+++ functions/checkAgroSolidDocument.js
@@ -3,14 +3,14 @@ import {getDocument} from "./getDocument";
 import {initializeAgroSolidOwl} from "./initializeAgroSolidOwl";
 
 export const checkAgroSolidDocument = (webId) => {
-  let webIdRoot = `${webId.split(".net/")[0]}.net/`
-  let documentUri = `${webIdRoot}agrosolid`;
+  let webIdRoot = `${webId.split("/profile/card#me")[0]}`
+  let documentUri = `${webIdRoot}/agrosolid`;
   getDocument(documentUri).then(
     async (document) => {
       if (!document) {
         const document = createDocument(documentUri);
         await document.save();
-        await initializeAgroSolidOwl(`${webIdRoot}agrosolid.owl`);
+        await initializeAgroSolidOwl(`${webIdRoot}/agrosolid.owl`);
       }
     }
   )
