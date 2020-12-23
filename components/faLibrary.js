5e27d13f8bbee72473a372e44018b55e6737e8bc
Alejo Marin Iparraguirre <alejomariniparraguirre@gmail.com>
2020-12-22 23:36:10 -0300

    faIcons y borrado globals.css

---
+++ components/faLibrary.js
@@ -0,0 +1,9 @@
+import {library} from '@fortawesome/fontawesome-svg-core'
+import {faHiking, faTractor, faSeedling, faBox} from '@fortawesome/free-solid-svg-icons'
+
+library.add(
+  faHiking,
+  faTractor,
+  faSeedling,
+  faBox,
+);
