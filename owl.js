2791172c201cf35c5aa84fdb4d4c3697a1d293b8
Alejo Marin Iparraguirre <alejomariniparraguirre@gmail.com>
2020-12-24 01:16:59 -0300

    owl en el sistema

---
+++ owl.js
@@ -0,0 +1,27 @@
+export const Crop = "https://agrosolid.inrupt.net/agrosolid.owl#Crop";
+
+export const Event = "https://agrosolid.inrupt.net/agrosolid.owl#Event";
+
+export const Farm = "https://agrosolid.inrupt.net/agrosolid.owl#Farm";
+
+export const Harvesting = "https://agrosolid.inrupt.net/agrosolid.owl#Harvesting";
+
+export const Plot = "https://agrosolid.inrupt.net/agrosolid.owl#Plot";
+
+export const Sowing = "https://agrosolid.inrupt.net/agrosolid.owl#Sowing";
+
+export const name = "https://agrosolid.inrupt.net/agrosolid.owl#name";
+
+export const quantity = "https://agrosolid.inrupt.net/agrosolid.owl#quantity";
+
+export const surface = "https://agrosolid.inrupt.net/agrosolid.owl#surface";
+
+export const timestamp = "https://agrosolid.inrupt.net/agrosolid.owl#timestamp";
+
+export const hasCrop = "https://agrosolid.inrupt.net/agrosolid.owl#hasCrop";
+
+export const hasEvent = "https://agrosolid.inrupt.net/agrosolid.owl#hasEvent";
+
+export const hasOwner = "https://agrosolid.inrupt.net/agrosolid.owl#hasOwner";
+
+export const hasPlot = "https://agrosolid.inrupt.net/agrosolid.owl#hasPlot";
