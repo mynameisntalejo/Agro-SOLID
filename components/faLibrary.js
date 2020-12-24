5377bc5c554e8dfb06f978271e7b1dfdd1642fde
Alejo Marin Iparraguirre <alejomariniparraguirre@gmail.com>
2020-12-24 16:06:13 -0300

    Incorporación de iconos para su uso

--- components/faLibrary.js
+++ components/faLibrary.js
@@ -1,9 +1,11 @@
 import {library} from '@fortawesome/fontawesome-svg-core'
-import {faHiking, faTractor, faSeedling, faBox} from '@fortawesome/free-solid-svg-icons'
+import {faHiking, faTractor, faSeedling, faBox, faPlus, faMinus} from '@fortawesome/free-solid-svg-icons'
 
 library.add(
   faHiking,
   faTractor,
   faSeedling,
   faBox,
+  faPlus,
+  faMinus,
 );
