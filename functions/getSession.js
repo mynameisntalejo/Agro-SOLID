c68042cedda0d43913e0e5667cd1e1a08e6c28b1
Alejo Marin Iparraguirre <alejomariniparraguirre@gmail.com>
2020-12-30 01:54:56 -0300

    Agregado de usuarios a documento de agrosolid

--- functions/getSession.js
+++ functions/getSession.js
@@ -1,10 +1,14 @@
 import auth from "solid-auth-client";
+import {saveUser} from "./saveUser";
 
 export const getSession = async () => {
   let tracksession = null;
   await auth.trackSession(
     (session) => {
       tracksession = session;
+      if (tracksession) {
+        saveUser(tracksession.webId);
+      }
     }
   );
   return tracksession;
---
+++ functions/saveUser.js
@@ -0,0 +1,17 @@
+import {rdfs} from "rdf-namespaces";
+import {getDocument} from "./getDocument";
+
+export const saveUser = async (webId) => {
+  let usersDocument = await getDocument("https://agrosolid.inrupt.net/usersWebId.ttl");
+  if (usersDocument) {
+    let exists = false;
+    for (const user of usersDocument.findSubjects(rdfs.isDefinedBy, webId)) {
+      exists = true;
+    }
+    if (!exists) {
+      let userSubject = usersDocument.addSubject();
+      userSubject.setRef(rdfs.isDefinedBy, webId);
+      await usersDocument.save();
+    }
+  }
+};
